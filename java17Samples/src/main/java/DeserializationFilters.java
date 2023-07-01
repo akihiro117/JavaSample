1ac591bae3334d7e80f7d2a799cccec4664ab886
Akihiro Yamada <u.parr917@gmail.com>
2023-07-02 02:04:19 +0900

    Context-Specific Deserialization Filters

---
+++ java17Samples/src/main/java/DeserializationFilters.java
@@ -0,0 +1,14 @@
+import java.util.HexFormat;
+
+public class DeserializationFilters {
+    public static void main(String[] args) {
+        // 0000000a
+        System.out.println(HexFormat.of().toHexDigits(10));
+        // 0000000A
+        System.out.println(HexFormat.of().withUpperCase().toHexDigits(10));
+        // 171
+        System.out.println(HexFormat.fromHexDigits("ab"));
+        // #01,#0a,#03
+        System.out.println(HexFormat.ofDelimiter(",").withPrefix("#").formatHex(new byte[]{1,10,3}));
+    }
+}
